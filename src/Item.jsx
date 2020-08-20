0ea9bc40ad08854054a8a15e7bcc3a64c8bdd624
Schnecke <eiweala@live.com>
2020-08-20 12:21:16 +0800

    simulate item click

--- src/Item.jsx
+++ src/Item.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
 
 export default class Item extends Component {
+  state = {
+    show: false,
+  };
+
+  showPriceInformation = () => {
+    this.setState({ show: !this.state.show });
+    // this.setState(prevState => ({show : !prevState.show}))
+  };
+
   render() {
     return (
       <div>
         <h1>Item</h1>
+        <button onClick={this.showPriceInformation} className="showPrice">
+          show price
+        </button>
+        {this.state.show && <div className="itemPrice">price</div>}
       </div>
     );
   }
