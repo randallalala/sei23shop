1613b65a39201474589eed3e018b4fda98adfda0
Schnecke <eiweala@live.com>
2020-08-21 15:43:22 +0800

    Updates

---
+++ precache-manifest.1817376663fd32c7badc4124f603cca2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85cb3d8f7ac799ce9176b21d6c68021f",
+    "url": "/sei23shop/index.html"
+  },
+  {
+    "revision": "1e328562fd50e7c2ccb3",
+    "url": "/sei23shop/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "d4e2727e99be85ce5e58",
+    "url": "/sei23shop/static/css/main.533350b4.chunk.css"
+  },
+  {
+    "revision": "1e328562fd50e7c2ccb3",
+    "url": "/sei23shop/static/js/2.ee657cd9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/sei23shop/static/js/2.ee657cd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4e2727e99be85ce5e58",
+    "url": "/sei23shop/static/js/main.a093eac5.chunk.js"
+  },
+  {
+    "revision": "b2389a408f9e31bc235e",
+    "url": "/sei23shop/static/js/runtime-main.d0965ef4.js"
+  }
+]);
